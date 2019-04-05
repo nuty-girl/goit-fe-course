d1c7a3f287cc8b96bab996e79a09f1c837b7bbaa
unknown <nuty-girl@mail.ru>
2019-04-05 20:39:23 +0300

    Home work01 JS

---
+++ javascript/modul-01/src/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/* const ADMIN_PASSWORD = 'm4ng0h4ckz';
+let message1;
+const input1 = prompt('Введите Ваш пароль');
+if (input1 === null) {
+  message1 = 'Отмененно пользователем!';
+} else if (input1 === ADMIN_PASSWORD) {
+  message1 = 'Добро пожаловать!';
+} else if (input1 !== ADMIN_PASSWORD) {
+  message1 = 'Доступ запрещен, неверный пароль!';
+}
+alert(message1);
+
+//--------задание 2
+
+const credits = 23580;
+let countDroid;
+const pricePerDroid = 3000;
+let totalPrice;
+let message;
+const input = prompt('Укажите количество дроидов, которое Вы хотите купить');
+if (input === null) {
+  message = 'Отменено пользователем!';
+  console.log(message);
+} else {
+  totalPrice = pricePerDroid * input;
+  if (totalPrice > credits) {
+    message = 'Недостаточно средств на счету!';
+    console.log(message);
+  } else {
+    let balance;
+    balance = credits - totalPrice;
+    let resumeMessage = `Вы купили ${input} дроидов, на счету осталось ${balance} кредитов.`;
+    alert(resumeMessage);
+    //console.log(resumeMessage);
+  }
+} */
+
+//------задание 3
+
+const country = prompt('Укажите страну для доставки товара:').toLowerCase();
+let price;
+
+switch (country) {
+  case 'китай':
+    price = '100';
+    break;
+  case 'южная америка':
+    price = '250';
+    break;
+  case 'австралия':
+    price = '170';
+    break;
+  case 'индия':
+    price = '80';
+    break;
+  case 'ямайка':
+    price = '120';
+    break;
+  default:
+    console.log('В вашей стране доставка не доступна');
+}
+const message3 = `Доставка в ${country} будет стоить ${price}  кредитов`;
+if (country === true) {
+  console.log(message3);
+}
