7f237cbce8a955927a59eed359fd1320fcbf08ff
unknown <nuty-girl@mail.ru>
2019-04-22 21:43:56 +0300

    HW01 task-3 fixed

--- javascript/modul-01/src/app.js
+++ javascript/modul-01/src/app.js
@@ -1,65 +1,73 @@
 'use strict';
 
-/* const ADMIN_PASSWORD = 'm4ng0h4ckz';
-let message1;
-const input1 = prompt('Введите Ваш пароль');
-if (input1 === null) {
-  message1 = 'Отмененно пользователем!';
-} else if (input1 === ADMIN_PASSWORD) {
-  message1 = 'Добро пожаловать!';
-} else if (input1 !== ADMIN_PASSWORD) {
-  message1 = 'Доступ запрещен, неверный пароль!';
+const ADMIN_PASSWORD = 'm4ng0h4ckz';
+let message;
+const userInput = prompt('Введите Ваш пароль');
+if (userInput === null) {
+  message = 'Отмененно пользователем!';
+} else if (userInput === ADMIN_PASSWORD) {
+  message = 'Добро пожаловать!';
+} else {
+  message = 'Доступ запрещен, неверный пароль!';
 }
-alert(message1);*/
+alert(message);
 
-//--------задание 2
+///////--------задание 2
 
-// const credits = 23580;
-// let countDroid;
-// const pricePerDroid = 3000;
-// let totalPrice;
-// const input = prompt('Укажите количество дроидов, которое Вы хотите купить');
-// if (input === null) {
-//   console.log('Отменено пользователем!');
-// } else {
-//   totalPrice = pricePerDroid * input;
-//   if (totalPrice > credits) {
-//     console.log('Недостаточно средств на счету!');
-//   } else {
-//     console.log(`Вы купили ${input} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
-//   }
-// }
+const credits = 23580;
+let countDroid;
+const pricePerDroid = 3000;
+let totalPrice;
+const customerInput = prompt(
+  'Укажите количество дроидов, которое Вы хотите купить',
+);
+if (customerInput === null) {
+  console.log('Отменено пользователем!');
+} else {
+  totalPrice = pricePerDroid * customerInput;
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    console.log(
+      `Вы купили ${customerInput} дроидов, на счету осталось ${credits -
+        totalPrice} кредитов.`,
+    );
+  }
+}
 
-///------задание 3
+// ///------задание 3
 
-let country;
 const input = prompt('Укажите страну для доставки товара:');
-if (input !== null) {
-  country = input.toLowerCase();
-}
-let price;
+if (input === null) {
+  console.log('Отменено пользователем!');
+} else {
+  const priceChina = 100;
+  const priceSouthAmerica = 250;
+  const priceAustralia = 170;
+  const priceIndia = 80;
+  const priceJamaica = 120;
 
-switch (country) {
-  case 'китай':
-    price = 100;
-    console.log(`Доставка в ${country} будет стоить ${price}  кредитов`);
-    break;
-  case 'южная америка':
-    price = 250;
-    console.log(`Доставка в ${country} будет стоить ${price}  кредитов`);
-    break;
-  case 'австралия':
-    price = 170;
-    console.log(`Доставка в ${country} будет стоить ${price}  кредитов`);
-    break;
-  case 'индия':
-    price = 80;
-    console.log(`Доставка в ${country} будет стоить ${price}  кредитов`);
-    break;
-  case 'ямайка':
-    price = 120;
-    console.log(`Доставка в ${country} будет стоить ${price}  кредитов`);
-    break;
-  default:
-    console.log('В вашей стране доставка не доступна');
+  switch (input.toLowerCase()) {
+    case 'китай':
+      console.log(`Доставка в китай будет стоить ${priceChina}  кредитов`);
+      break;
+    case 'южная америка':
+      console.log(
+        `Доставка в южную америку будет стоить ${priceSouthAmerica}  кредитов`,
+      );
+      break;
+    case 'австралия':
+      console.log(
+        `Доставка в австралию будет стоить ${priceAustralia}  кредитов`,
+      );
+      break;
+    case 'индия':
+      console.log(`Доставка в индию будет стоить ${priceIndia}  кредитов`);
+      break;
+    case 'ямайка':
+      console.log(`Доставка в ямайку будет стоить ${priceJamaica}  кредитов`);
+      break;
+    default:
+      console.log('В вашей стране доставка не доступна');
+  }
 }
