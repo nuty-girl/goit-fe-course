bc1be7d571feab62f677a201a6499ee4d6034020
unknown <nuty-girl@mail.ru>
2019-05-02 22:02:57 +0300

    HomeWork 03

---
+++ javascript/modul-03/src/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function(login) {
+  return login.length >= 4 && login.length <= 16;
+};
+
+const isLoginUnique = function(logins, login) {
+  return logins.includes(login);
+};
+
+const addLogin = function(logins, login) {
+  if (!isLoginValid(login)) {
+    console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+  } else if (isLoginUnique(logins, login)) {
+    console.log('Такой логин уже используется!');
+  } else {
+    logins.push(login);
+    console.log('Логин успешно добавлен!');
+  }
+};
+
+// Вызовы функции для проверки
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
